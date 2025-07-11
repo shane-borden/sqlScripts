2fa2ef679f1beacaac5db6f4c0a5045be66ea7e3
Shane Borden <sborden76@gmail.com>
2025-07-11 10:12:13 -0400

    Update postgres_table_object_size.sql

--- postgres/postgres_table_object_size.sql
+++ postgres/postgres_table_object_size.sql
@@ -24,16 +24,18 @@ FROM (
   FROM (
     SELECT
       c.oid,
-      nspname AS table_schema,
-      relname AS table_name,
+      n.nspname AS table_schema,
+      c.relname AS table_name,
       c.reltuples AS row_estimate,
+      pct.relname AS toast_table_name,
       pg_total_relation_size(c.oid) AS total_bytes,
       pg_indexes_size(c.oid) AS index_bytes,
-      pg_total_relation_size(reltoastrelid) AS toast_bytes
+      pg_total_relation_size(c.reltoastrelid) AS toast_bytes
     FROM
       pg_class c
+      JOIN pg_class pct ON (c.reltoastrelid = pct.oid)
       LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
-    WHERE relkind = 'r'
+    WHERE c.relkind = 'r'
   ) a
 ) a
 WHERE table_schema = 'public'
